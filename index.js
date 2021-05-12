6b0dafb2d0c2f4460461b518a1b51db4005ca98b
Krae <kraewind@gmail.com>
2021-05-12 16:56:13 -0400

    commit tree deb6c05da14ddc277da24f9fee9356407f9d1217

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const superbowlWin = (array) => {
+    let fg = array.find( function(s) { return s.result === "W" })
+    if (fg) {
+        return fg['year']
+    }
+    else {
+        return undefined
+    }
+}
